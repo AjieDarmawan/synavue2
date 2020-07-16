7a6e76f0560f26faaa9cc5dacd984a483d52973a
ajiedarmawan <ajie.darmawan106@gmail.com>
2020-07-16 09:37:00 +0700

    tes

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+      <HeaderSyana/>
+      <HeroSyana/>
+      <BannerSyana/>
+      <InstaSyana/>
+      <PartnerSyana/>
+      <FooterSyana/>
+      
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderSyana from '@/components/headerSyana.vue'
+import HeroSyana from '@/components/heroSyana.vue'
+import BannerSyana from '@/components/bannerSyana.vue'
+import InstaSyana from '@/components/instaSyana.vue'
+import PartnerSyana from '@/components/partnerSyana.vue'
+import FooterSyana from '@/components/footerSyana.vue'
+
+
+
+export default {
+  name: 'Home',
+  components: {
+   HeaderSyana,
+   HeroSyana,
+   BannerSyana,
+   InstaSyana,
+   PartnerSyana,
+   FooterSyana
+
+  }
+}
+</script>
