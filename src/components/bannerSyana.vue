055b703cc61e14cce596dc791dac8c9aa8405c96
ajiedarmawan <ajie.darmawan106@gmail.com>
2020-07-17 15:19:38 +0700

    tes

--- src/components/bannerSyana.vue
+++ src/components/bannerSyana.vue
@@ -2,11 +2,11 @@
     <section class="women-banner spad">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12 mt-5">
-                    <carousel class="product-slider" :loop="true" :autoplay="true" :items="3" :dots="false" :nav="false">>
-                        <div class="product-item">
+                <div class="col-lg-12 mt-5" v-if="products.length > 0">
+                    <carousel class="product-slider"  :autoplay="true" :items="3" :dots="false" :nav="false">>
+                        <div class="product-item" v-for="productItem in products" v-bind:key="productItem.id">
                             <div class="pi-pic">
-                                   <img src="img/products/women-1.jpg" alt="" />
+                                   <img v-bind:src="productItem.galleries[0].photo" alt="" />
                                 <ul>
                                     <li class="w-icon active">
                                         <a href="#"><i class="icon_bag_alt"></i></a>
@@ -17,81 +17,23 @@
                                 </ul>
                             </div>
                             <div class="pi-text">
-                                <div class="catagory-name">Coat</div>
+                                <div class="catagory-name">{{productItem.type}}</div>
                                 <router-link to="/product">
                                 <a href="#">
-                                    <h5>Mickey Baggy</h5>
+                                    <h5>{{productItem.name}}</h5>
                                 </a></router-link>
                                 <div class="product-price">
-                                    $14.00
-                                    <span>$35.00</span>
+                                   {{productItem.price}}
+                                    <span>{{productItem.price}}</span>
                                 </div>
                             </div>
                         </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-2.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Shoes</div>
-                                <a href="#">
-                                    <h5>Guangzhou sweater</h5>
-                                </a>
-                                <div class="product-price">
-                                    $13.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-3.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Pure Pineapple</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-4.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    <li class="w-icon">
-                                        <a href="#"><i class="fa fa-random"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Converse Shoes</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
+                       
                     </carousel>
+                </div>
+
+                <div class="col-lg-12" v-else>
+                    <p>Product Terbaru Belum Tersedia</p>
                 </div>
             </div>
         </div>
@@ -100,10 +42,22 @@
 
 <script>
 import carousel from 'vue-owl-carousel'
+import axios from 'axios'
+
 export default {  
     name:'bannerSyana',
       components:{
          carousel
+     },
+     data(){
+         return{
+             products:[]
+         }
+     },
+     mounted(){
+         axios.get('http://shayna-backend.belajarkoding.com/api/products')
+         .then(res=>(this.products = res.data.data.data))
+         .catch(err=>console.log(err));
      }
 }
 </script>
